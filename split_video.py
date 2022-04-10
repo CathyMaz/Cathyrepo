8bbf67a0e26a5f93045bd9c6b1777ad55165d56f
CathyMaz <103260684+CathyMaz@users.noreply.github.com>
2022-04-10 11:13:08 +0200

    Add files via upload

---
+++ split_video.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import os
+from keras.applications.vgg16 import preprocess_input
+
+def split_videos_to_frames(path:str):
+    
+    cap = cv2.VideoCapture(path)
+    
+    if cap.isOpened():
+        
+        print("Video Opened")
+        
+    try:
+        if not os.path.exists('./frames/'):
+            
+            os.makedirs('./frames/',exist_ok=True)
+            
+    except OSError:
+        
+        print ('Error: Creating directory of data')
+        
+    currentFrame = 8
+    
+    while(cap.isOpened()):    
+        
+            status, frame = cap.read()
+            
+            name = './frames/' + str(currentFrame) + '.jpg'
+            
+            print ('Creating...' + name)
+            
+            if status:
+                
+                cv2.imwrite(name, frame)
+                
+            currentFrame += 1
+            
+            if currentFrame == 20:
+                
+                break
+            
+    cap.release()
+    
+    
+    
+
+#cv2.destroyAllWindows()
+    
